f99622a112815d264ae81e19f2cc9963436f94aa
Breant <jeremy.breant@edu.itescia.fr>
2021-09-22 12:49:41 +0200

    Update with alan

--- finances/src/companies/companies.service.ts
+++ finances/src/companies/companies.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { Company } from './entities/company.entity';
+import { Stats } from './entities/stats.entity';
 
 @Injectable()
 export class CompaniesService {
+
+  companies:Company[] = [];
+
   create(createCompanyDto: CreateCompanyDto) {
+    const newCompany = new Company("toto","company1",new Stats(10,34,23));
+    this.companies.push(newCompany);
+
     return 'This action adds a new company';
   }
 
   findAll() {
-    return `This action returns all companies`;
+    return this.companies;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
+  findOne(name: string) {
+    const company = this.companies.find(element => element.name = name)
+    if(!company){
+      throw new NotFoundException;
+    }
+    return {...company};
   }
 
   update(id: number, updateCompanyDto: UpdateCompanyDto) {
--- finances/src/companies/entities/company.entity.ts
+++ finances/src/companies/entities/company.entity.ts
@@ -1,1 +1,13 @@
-export class Company {}
+import { Stats } from "./stats.entity";
+
+export class Company {
+    id: string;
+    name: string;
+    stats: any;
+
+    constructor(id: string, name: string, stats: Stats){
+        this.id = id;
+        this.name = name;
+        this.stats = stats;
+    }
+}   
