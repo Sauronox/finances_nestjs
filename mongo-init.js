458c1690eacf8fec1f6f9594b604827a4664dc70
Alan Drieux <alan.drieux@outlook.fr>
2021-09-23 17:30:28 +0200

    add file to init database finance mongo

---
+++ mongo-init.js
@@ -0,0 +1,22 @@
+const username = _getEnv('DB_FINANCE_USER')
+const password = _getEnv('DB_FINANCE_PASSWORD')
+const db_name = _getEnv('DB_NAME')
+
+
+dbf = db.getSiblingDB(db_name);
+
+dbf.createCollection('blank');
+
+dbf.createUser(
+    {
+        user: username,
+        pwd: password,
+        roles: [
+            {
+                role: "readWrite",
+                db: db_name
+            }
+        ]
+    }
+);
+dbf.blank.insert({ myfield: 'initialisation'})
