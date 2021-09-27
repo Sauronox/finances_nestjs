928eca4fc7ccf8f2c43e1b4778f697f255cf523e
Nono <arnaud.chretien@outlook.com>
2021-09-27 15:14:23 +0200

    making the api call

--- finances/src/fetch-api/fetch-api.controller.ts
+++ finances/src/fetch-api/fetch-api.controller.ts
@@ -1,24 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get } from '@nestjs/common';
 import { FetchApiService } from './fetch-api.service';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
 
 @Controller('fetch-api')
 export class FetchApiController {
-  constructor(private readonly fetchApiService: FetchApiService) {}
-
+  constructor(
+    private readonly fetchApiService: FetchApiService,
+    private httpService: HttpService,
+  ) {}
   @Get('/fetch')
   findAll() {
-      try {
-        return this.httpService.get('/').pipe(
-            map((result) => {
-              this.logger.debug(result.data);
-              return result.data;
-            }),
-        );
-      } catch (error) {
-        this.logger.debug(error);
-        return new BadRequestException(error);
-      }
+    try {
+      return this.httpService.get('/stock/metric?symbol=AAPL&metric=all').pipe(
+        map((result) => {
+          return result.data;
+        }),
+      );
+    } catch (error) {
+      return new BadRequestException(error);
     }
-  }
   }
 }
--- finances/src/fetch-api/fetch-api.module.ts
+++ finances/src/fetch-api/fetch-api.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FetchApiController } from './fetch-api.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [FetchApiController]
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: '',
+        headers: {},
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [FetchApiController],
 })
 export class FetchApiModule {}
