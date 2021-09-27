e2d45ce73fe23892259dae14d1db193a42a5a14c
Nonorinotito <80772876+Nonorinotito@users.noreply.github.com>
2021-09-27 17:58:31 +0200

    Feature/fetch api (#4)
    * init fetch api service
    
    * fixing wrong emplacement
    
    * adding controller
    
    * adding axios to the project
    
    * making the api call
    
    * no more conf bugs
    
    * finnhub call works
    
    * Adapting the route to get the mnemonic
    
    * removing from path to transform as a service
    
    * hided in .env
    
    Co-authored-by: Nono <arnaud.chretien@outlook.com>

--- finances/src/app.module.ts
+++ finances/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FetchApiService } from './fetch-api/fetch-api.service';
+import { FetchApiModule } from './fetch-api/fetch-api.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FetchApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ finances/src/fetch-api/fetch-api.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { FetchApiController } from './fetch-api.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FetchApiService } from './fetch-api.service';
+
+@Module({
+  imports: [
+    ConfigModule,
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get('FINNHUB_APIENDPOINT'),
+        headers: { 'X-Finnhub-Token': configService.get('FINNHUB_APIKEY') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [FetchApiController],
+  providers: [FetchApiService],
+})
+export class FetchApiModule {}
