a7f4e8b7317ae972da60922c9b726f5cacd67143
Nono <arnaud.chretien@outlook.com>
2021-09-27 17:07:53 +0200

    removing from path to transform as a service

--- finances/src/fetch-api/fetch-api.controller.ts
+++ finances/src/fetch-api/fetch-api.controller.ts
@@ -1,32 +1,14 @@
-import {BadRequestException, Controller, Get, Param} from '@nestjs/common';
+import { BadRequestException, Controller, Get } from '@nestjs/common';
 import { FetchApiService } from './fetch-api.service';
-import { HttpService } from '@nestjs/axios';
-import { map } from 'rxjs';
-import { plainToClass } from 'class-transformer';
-import { ApiStockModel } from './Models/ApiStockModel';
 
 @Controller('fetch')
 export class FetchApiController {
-  constructor(
-    private readonly fetchApiService: FetchApiService,
-    private httpService: HttpService,
-  ) {}
-  @Get(':mnemonic')
-  findAll(@Param('mnemonic') mnemonic: string) {
-    try {
-      return this.httpService.get('/stock/metric?symbol='+mnemonic+'&metric=all').pipe(
-        map((result) => {
-          console.log(result.data);
-          const fetchedValue = result.data.metric;
-          return new ApiStockModel(
-            fetchedValue.dividendYieldIndicatedAnnual,
-            fetchedValue.netProfitMarginTTM,
-            fetchedValue.roeTTM,
-          );
-        }),
-      );
-    } catch (error) {
-      return new BadRequestException(error);
+    constructor(
+        private readonly fetchApiService: FetchApiService,
+
+    ) {}
+    @Get()
+    findAll() {
+        return this.fetchApiService.fetchAllStock();
     }
-  }
 }
--- finances/src/fetch-api/fetch-api.service.ts
+++ finances/src/fetch-api/fetch-api.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ApiStockModel } from './Models/ApiStockModel';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
-export class FetchApiService {}
+export class FetchApiService {
+  constructor(private httpService: HttpService) {}
+
+  async fetchAllStock() {
+    const allStocks: string[] = ['AAPL','AMZN','FB','GOOGL'];
+    let allStocksFetched: ApiStockModel[] = [];
+    for (const element of allStocks) {
+      const stock = await this.fetchOneStock(element);
+      console.log(element);
+      console.log(stock);
+      allStocksFetched.push(stock);
+    }
+    console.log(allStocksFetched);
+    return allStocksFetched;
+  }
+
+  async fetchOneStock(mnemonic: string) {
+    try {
+      return lastValueFrom(
+        this.httpService
+          .get('/stock/metric?symbol=' + mnemonic + '&metric=all')
+          .pipe(
+            map((result) => {
+              const fetchedValue = result.data.metric;
+              return new ApiStockModel(
+                fetchedValue.dividendYieldIndicatedAnnual,
+                fetchedValue.netProfitMarginTTM,
+                fetchedValue.roeTTM,
+              );
+            }),
+          ),
+      );
+    } catch (error) {
+      throw new Error('mnemonic fail');
+    }
+  }
+}
